b2e8fc964496195971ea23d5a5a5adf4e13979c3
grain-sand <Grain_Of_Sand@outlook.com>
2026-02-10 16:49:32 +0800

    调整 tsconfig 默认值

--- src/main/initProject.ts
+++ src/main/initProject.ts
@@ -6,15 +6,20 @@ const tsCfgPath = 'tsconfig.json'
 
 const tsConfig = {
 	compilerOptions: {
+		allowJs: true,
+		baseUrl: "./",
 		esModuleInterop: true,
 		experimentalDecorators: true,
-		preserveConstEnums: true,
-		module: "ESNext",
-		moduleResolution: "bundler",
-		sourceMap: false,
-		target: "ESNext",
+		forceConsistentCasingInFileNames: true,
 		lib: ["ESNext", "DOM"],
-		allowJs: true
+		module: "ESNext",
+		moduleResolution: "Bundler",
+		noEmit: true,
+		preserveConstEnums: true,
+		skipLibCheck: true,
+		sourceMap: false,
+		strict: false,
+		target: "ESNext",
 	}
 }
 
@@ -39,7 +44,7 @@ function writeCfg(args: string[]) {
 	if (existsSync(cfgPath)) {
 		console.log('\x1b[32mrollup.config.ts already exists\x1b[0m')
 	}
-	const pattern = args.length>1 ? args.pop() : undefined;
+	const pattern = args.length > 1 ? args.pop() : undefined;
 	const detectedOption = detectRollupOption(pattern, true);
 	const out = [
 		"import { RollupOptions } from 'rollup'",
